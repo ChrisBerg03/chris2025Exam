db93c436667c5694523fb3adbc1beb0e4f7b01f2
DESKTOP-67BT1F3\Panda <chrisrberg03@gmail.com>
2025-04-09 15:54:03 +0200

    chore: basic setup of repository with tailwind and vite

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer className="bg-amber-600 text-white h-[75px] text-center mt-8">
+            <p>&copy; 2025 MyApp. All rights reserved.</p>
+        </footer>
+    );
+}
